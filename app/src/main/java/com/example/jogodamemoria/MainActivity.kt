67a6cc886e235f50a4ff70012ac48bca9ea64dd8
Eyenessus <eyenessus@gmail.com>
2022-02-27 14:50:26 -0300

    Jogo da memoria Android Studio

---
+++ app/src/main/java/com/example/jogodamemoria/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jogodamemoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var card1 = findViewById<ImageView>(R.id.image1)
+    }
+}
